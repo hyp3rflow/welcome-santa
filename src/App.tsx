b4dea91058867fb60ef6a2989ccb456f02620923
hyperflow <hyperflow@kakao.com>
2020-12-25 07:46:48 +0900

    refactor: 사용하지 않은 모듈 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.css';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as THREE from 'three';
-import { Geometry, GeometryUtils } from 'three';
 
 function App() {
   useEffect(() => {
